f48a8776ee2f7e19d51e311344a5efda05cfb0e4
Harsh Agrawal <sunfall6@gmail.com>
2020-05-28 20:10:44 +0530

    Changed checkname

--- train_custom.sh
+++ train_custom.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python3 train_autodeeplab.py \
- --batch-size 2 --dataset custom --checkname July29_newbetas_branch \
+ --batch-size 2 --dataset custom --checkname test_custom_1 \
  --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321
